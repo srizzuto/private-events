8914a3f3809722554fd37f97acd2173206bb6c27
Sebastian Rizzuto <srizzuto@gmail.com>
2021-06-08 06:50:41 -0300

    create method, add a new build for news events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,27 @@
 class EventsController < ApplicationController
+  
+  before_action authenticate_user, only: [:create, :new]
+
+  def new
+    @event = Event.new
+  end
+
   def index
+  end
+
+  def create
+    user = current_user
+    @event = user.build(event_params)
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/:id', to: 'users#show'
   devise_for :users
   get 'events/index'
   root "events#index"
