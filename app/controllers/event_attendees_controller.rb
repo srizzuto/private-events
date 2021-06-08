53b100176a9abbbd8027cbf0c430dd73c22ef896
Sebastian Rizzuto <srizzuto@gmail.com>
2021-06-08 11:25:27 -0300

    fix rubocop

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -4,6 +4,7 @@ class EventAttendeesController < ApplicationController
     if event
       @event_attendee = event.event_attendees.build(attendee_id: current_user.id)
       if @event_attendee.save
+        redirect_to event_path(events) and return
       end
       redirect_to event_path(event)
     end
