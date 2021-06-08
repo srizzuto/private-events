b2a8a1775448bf9f340e29a2e2a4627added6c30
Sebastian Rizzuto <srizzuto@gmail.com>
2021-06-08 06:37:36 -0300

    Add devise views

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+
+  belongs_to :creator, class_name: "User", source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :events, foreign_key: "creator_id", class_name: "Event"
 end
