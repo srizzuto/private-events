d9127701b156443d59f1cf038d149c5f85959c88
Sebastian Rizzuto <srizzuto@gmail.com>
2021-06-09 08:21:51 -0300

    fix rubocop, add auth test

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -2,12 +2,10 @@ class EventAttendeesController < ApplicationController
   def create
     event = Event.find(params[:event_id])
     attended = event.event_attendees.find_by(attendee_id: current_user.id)
-    if attended
-      redirect_to event_path(event) and return
-    else
-      @event_attendee = event.event_attendees.build(attendee_id: current_user.id)
-      redirect_to event_path(event) and return if @event_attendee.save
-    end
+    redirect_to event_path(event) and return if attended
+
+    @event_attendee = event.event_attendees.build(attendee_id: current_user.id)
+    redirect_to event_path(event) and return if @event_attendee.save
 
     redirect_to event_path(event)
   end
--- spec/authentication/logout_spec.rb
+++ spec/authentication/logout_spec.rb
@@ -1,15 +1,15 @@
-require 'capybara/rails'
-
-describe 'the signin process', type: :feature do
+require 'rails_helper'
+describe 'the signout process', type: :feature do
   before :each do
     User.create(email: 'user1@gmail.com', password: 'password', name: 'User1')
   end
-  it 'signs @user in' do
+  it 'Log out the user' do
     visit '/users/sign_in'
     fill_in 'Email', with: 'user1@gmail.com'
     fill_in 'Password', with: 'password'
     click_button 'Log in'
+    click_link 'Logout'
     expect(current_path).to eq(root_path)
-    expect(page).to have_text('Signed in successfully.')
+    expect(page).to have_text('Signed out successfully.')
   end
 end
